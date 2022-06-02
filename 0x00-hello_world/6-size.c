05e7361a1f18fafc43b6a6ca90696909c31c4937
Zaks0n <zaksont@gmail.com>
2022-06-02 04:32:22 -0700

    Debugged 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- *Main - prints the size of vriable types
- *Return: 0 for successful and vice-versa
+ * main - prints the size of vriable types
+ * Return: 0 for successful and vice-versa
  */
 int main(void)
 {
-                printf("Size of a char: %d byte(s)\n", sizeof(char));
-	        printf("Size of an int: %d byte(s)\n", sizeof(int));
-		printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-		printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-		printf("Size of a float: %d byte(s)\n", sizeof(float));
-		return (0); 
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0); 
 }
