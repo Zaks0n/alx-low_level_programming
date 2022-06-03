be67b198a01d369ceaaa07317cd177d7fb19285d
Zaks0n <zaksont@gmail.com>
2022-06-03 07:15:34 -0700

    Basic Tasks - 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints hexidecimal difits
+ *
+ * Return: 0 always.
+ */
+
+int main(void)
+{
+	int num;
+	char alph;
+
+	for (num = 0; num <= 10; num++)
+		putchar((num % 10) + '0');
+
+	for (alph = 'a'; alph <= 'f'; alph++)
+		putchar(alph);
+	putchar('\n');
+
+	return (0);
+}
