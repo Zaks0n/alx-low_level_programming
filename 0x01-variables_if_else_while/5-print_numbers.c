d4f899d67ca0d38cac547199e9df388eab71f65c
Zaks0n <zaksont@gmail.com>
2022-06-03 05:34:15 -0700

    Basic Tasks - 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  main - prints 1 digit integers.
+ *
+ *  Return: 0 always.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
