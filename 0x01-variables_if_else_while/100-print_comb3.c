898a4a3d8c8174dbfc38bb49b4a73ff2cdbd44da
Zaks0n <zaksont@gmail.com>
2022-06-05 07:35:45 -0700

    Advance Tasks - 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints all combinations of two digits
+ * Return: 0 always.
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 48; x <= 56; x++)
+	{
+		for (y = 49; y <= 57; y++)
+		{
+			putchar(x);
+			putchar(y);
+			if (x != 56 || y != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
