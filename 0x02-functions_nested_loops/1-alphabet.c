bb11e49461ac8ee9a667cf74c5a860ea80fc42ae
Zaks0n <zaksont@gmail.com>
2022-06-07 07:02:33 -0700

    Basic Tasks - 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase.
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+
+	_putchar('\n');
+}
