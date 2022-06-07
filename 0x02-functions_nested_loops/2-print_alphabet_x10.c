bb66ec697baa3807517786ce6534a695a23462e0
Zaks0n <zaksont@gmail.com>
2022-06-07 07:14:20 -0700

    Basic Tasks - 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints lowercase alphabets 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int counter = 0;
+	char alpha;
+	
+	while (counter <= 10)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+		counter++;
+	}
+}
