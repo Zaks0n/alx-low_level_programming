b137e571e3e579179582f3f9b5b18106d41cd17e
Zaks0n <zaksont@gmail.com>
2022-06-07 08:36:27 -0700

    Advanced Tasks - 100 to 104

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,14 +14,13 @@ int main(void)
 
 	for (counter = 1; counter <= (countto / 2); counter++)
 	{
-		printf("%li, %li ", a, b);
+		printf("%li, %li, ", a, b);
 		a += b;
 		b += a;
 	}
 	if (countto % 2 == 1)
 	{
 		printf("%li", a);
-		printf(", ");
 	}
 
 	printf("\n");
