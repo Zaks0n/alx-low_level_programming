54efe130acb4b1895cbe2495f4e30de803a597d9
Zaks0n <zaksont@gmail.com>
2022-06-13 03:05:57 -0700

    Basic Tasks - 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps value of two integers.
+ * @a: input 1
+ * @b: input 2
+ * Retuen: integers.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int n;
+
+	n = *a;
+	*a = *b;
+	*b = n;
+}
