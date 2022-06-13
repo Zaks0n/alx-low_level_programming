a7dc45f2616b40d005df0f6cc6ba6daf29c1deae
Zaks0n <zaksont@gmail.com>
2022-06-13 04:17:32 -0700

    Basic Tasks - 4-9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int count = 0;
 
-	for (; src[i] != '\0'; i++)
+	while (count >= 0)
 	{
-		dest[i] = src[i];
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
 	}
 	return (dest);
 }
