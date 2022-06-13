e2b5a067ffc241aca28568d4e0d546a25831c250
Zaks0n <zaksont@gmail.com>
2022-06-13 04:13:30 -0700

    Basic Tasks - 4-9

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,9 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < (n - 1); i++)
 	{
-		printf("%d, ", a[i]);
-		if (i == (n - 1))
-			printf("%d", a[n - 1]);
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
 	}
 	printf("\n");
 }
